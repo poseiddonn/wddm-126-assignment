2f488f9a9fcc654427dbac0031d88e5bd1985c58
Isaac <88962878+poseiddonn@users.noreply.github.com>
2025-12-09 23:18:22 -0500

    added my css stylesheet

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".searchForm");
+const input = document.querySelector("#search");
 
+const books = new URL("https://openlibrary.org/search.json");
+
+const sBooks = () => {
+  books.search = new URLSearchParams({
+    author: input.value,
+    // limit: 5,
+    fields: "title",
+  });
+
+  fetch(books)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sBooks();
+});
