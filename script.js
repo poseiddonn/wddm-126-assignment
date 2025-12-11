166f7337c75536112c1d3d2821c5068fef3238e1
Isaac <88962878+poseiddonn@users.noreply.github.com>
2025-12-11 00:31:07 -0500

    finished my project at my mvp level

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 const form = document.querySelector(".searchForm");
 const input = document.querySelector("#search");
+const title = document.querySelector(".title");
 
 const books = new URL("https://openlibrary.org/search.json");
 
 const sBooks = () => {
   books.search = new URLSearchParams({
     q: input.value,
-    fields: "title",
+    limit: 50,
+    fields: "title, author_name",
   });
-
+  clearPage();
   fetch(books)
     .then((res) => {
       return res.json();
     })
     .then((data) => {
-      console.log(data);
+      const books = data.docs.map((book) => ({
+        title: book.title,
+        authors: book.author_name,
+      }));
+      books.forEach((books) => {
+        const bookTitle = document.createElement("li");
+        bookTitle.textContent = books.title;
+        title.appendChild(bookTitle);
+      });
     });
+};
+
+const clearPage = () => {
+  title.innerHTML = "";
+  input.value = "";
 };
 
 form.addEventListener("submit", (e) => {
